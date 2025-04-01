80d0cfc7eac1728ae1f9353ac1d6090660785d64
user <user@mail.com>
2025-04-01 00:49:53 -0400

    implement NullRenderer

--- pkg/render/renderer.go
+++ pkg/render/renderer.go
@@ -1,7 +1,11 @@
 // pkg/render/renderer.go
 package render
 
-import "github.com/opd-ai/go-netrek/pkg/entity"
+import (
+	"log"
+
+	"github.com/opd-ai/go-netrek/pkg/entity"
+)
 
 // NullRenderer is a simple implementation of entity.Renderer.
 type NullRenderer struct {
@@ -9,27 +13,27 @@ type NullRenderer struct {
 
 // Clear implements entity.Renderer.
 func (d *NullRenderer) Clear() {
-	panic("unimplemented")
+	log.Println("Clear called")
 }
 
 // Present implements entity.Renderer.
 func (d *NullRenderer) Present() {
-	panic("unimplemented")
+	log.Println("Present called")
 }
 
 // RenderPlanet implements entity.Renderer.
 func (d *NullRenderer) RenderPlanet(planet *entity.Planet) {
-	panic("unimplemented")
+	log.Printf("RenderPlanet called for planet: %v", planet)
 }
 
 // RenderProjectile implements entity.Renderer.
 func (d *NullRenderer) RenderProjectile(projectile *entity.Projectile) {
-	panic("unimplemented")
+	log.Printf("RenderProjectile called for projectile: %v", projectile)
 }
 
 // RenderShip implements entity.Renderer.
 func (d *NullRenderer) RenderShip(ship *entity.Ship) {
-	panic("unimplemented")
+	log.Printf("RenderShip called for ship: %v", ship)
 }
 
 var nullRenderer entity.Renderer = &NullRenderer{}
